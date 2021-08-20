fea935bb896ccc8188286cdf916468af12b8542b
Khushi Sharma <70655479+itskhushis1@users.noreply.github.com>
2021-08-20 13:05:07 +0530

    Update shufflecards.py

--- shufflecards.py
+++ shufflecards.py
@@ -9,6 +9,7 @@ deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
 random.shuffle(deck)
 
 # draw cards
+
 print("You got:")
 for i in range(5):
    print(deck[i][0], "of", deck[i][1])
